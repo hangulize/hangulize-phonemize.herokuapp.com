c856e0815c5309dfee136749b09aeed29df12d0a
Heungsub Lee <sub@subl.ee>
2018-10-04 12:57:20 +0900

    Accept slash in word

--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+// paramWord picks the word argument.
+//
+// The parameter should be declared as *word.
+//
+// (This function is stolen from api.hangulize.org)
+//
+func paramWord(c *gin.Context) string {
+	word := c.Param("word")
+
+	// Remove the initial slash.
+	word = word[1:]
+
+	unescaped, err := url.QueryUnescape(word)
+	if err != nil {
+		word = unescaped
+	}
+
+	return word
+}
+
 func handler(c *gin.Context) {
 	pID := c.Param("phonemizer")
-	word := c.Param("word")
+	word := paramWord(c)
 
 	p, ok := phonemizers[pID]
 
@@ -24,7 +45,7 @@ func handler(c *gin.Context) {
 }
 
 func register(r gin.IRouter) {
-	r.GET("/:phonemizer/:word", handler)
+	r.GET("/:phonemizer/*word", handler)
 }
 
 func main() {
